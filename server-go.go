d314ad2c818237c0180e4caeb329878908e7bc0b
harryalvarado <harry2alvarado8@gmail.com>
2018-09-07 13:20:51 -0500

    Se crea mini-servidor en GoLang con el framework Echo.

---
+++ server-go.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo"
+)
+
+func main() {
+	const VAR_PORT = ":4321" // Cont. indicador del puerto
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hola, Mundo!")
+	})
+	e.Logger.Fatal(e.Start(VAR_PORT))
+}
